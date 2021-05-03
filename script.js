712c1a10c1a328e8a4c7dc09cc57e40bb4608034
rahul007cdr <rahulrnair007@outlook.com>
2021-05-03 19:59:12 +0530

    commit for dark-mode

---
+++ script.js
@@ -0,0 +1,25 @@
+let dark=document.querySelector('.dark-btn');
+let body=document.body
+
+dark.addEventListener('click',function(e){
+    console.log('rahul')
+    e.preventDefault()
+    console.log(e)
+    body.classList.toggle('dark')
+
+    e.target.textContent=="dark"?e.target.textContent="light":e.target.textContent="dark";
+
+    if(body.classList.contains('dark')){
+        localStorage.setItem('darkMode','enabled');
+    }else{
+        localStorage.setItem('darkMode','disabled');
+    }
+ 
+})
+if(localStorage.getItem('darkMode')=='enabled'){
+    body.classList.toggle('dark')
+    // e.target.textContent=="dark"?e.target.textContent="light":e.target.textContent="dark";
+    dark.textContent="light"
+}else{
+    dark.textContent="dark"
+}
